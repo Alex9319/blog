8494149e09e26a554923579bcef61c5bc0a70de6
Alejandro Arévalo <alejandro19193@gmail.com>
2022-08-30 23:07:46 +0200

    List of post

--- src/Controller/Post/listController.php
+++ src/Controller/Post/listController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Post;
 
+use App\Service\ListService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,10 @@ class listController extends AbstractController
     #[Route('/list', name: 'app_post_list')]
     public function index(): JsonResponse
     {
-        $result = [
-            'message'    => '' ,
-            'status'     => false,
-            'statusCode' => 400
-        ];
+        $listPost = new ListService();
 
-        return $this->json($result);
+        $response = $listPost->listPost();
+
+        return $this->json($response);
     }
 }
